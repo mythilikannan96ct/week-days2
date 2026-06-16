feb77a54cdf7c1c26bdf81ee84023b639b39c9bc
mythilikannan96ct <mythilikannan96ct@gmail.com>
2026-06-16 22:57:53 +0530

    twolaunchbrowers.spec.ts2

--- twolaunchbrowers.spec.ts
+++ twolaunchbrowers.spec.ts
@@ -1,4 +1,4 @@
-import {test,chromium,webkit,Browser,Page}  from '@playwright/test';
+import {test,chromium,webkit, firefox,Browser,Page}  from '@playwright/test';
 test('Launch to Browser',async ()=>{
    //Edge browser instance
    const edgeBrowser:Browser= await chromium.launch({
@@ -23,9 +23,23 @@ test('Launch to Browser',async ()=>{
    console.log("flipkart title:", await webkitPage.title());
    console.log("flipkart URL:",webkitPage.url());
 
+   //firefox Browser instance
+
+   const firefoxBrowser: Browser= await firefox.launch({
+     headless: false
+
+   });
+   const firefoxPage: Page= await firefoxBrowser.newPage();
+   await firefoxPage.goto('https://www.amazon.in/');
 
 
-   await edgepage.waitForTimeout(2000);
+   console.log("amazon title:", await firefoxPage.title());
+   console.log("amazon URL:", firefoxPage.url());
+
+
+
+   await edgepage.waitForTimeout(3000);
    await edgeBrowser.close();
    await webkitBrowser.close();
+   await firefoxBrowser.close();
 });
